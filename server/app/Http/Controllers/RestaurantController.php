ea92519aedb0e8c102fc5bc74cdeb9f2815126f9
sahokawamura <saho_kawamura@fitnyc.edu>
2020-11-01 11:24:25 +0900

    カテゴリーを別テーブルで作成

--- server/app/Http/Controllers/RestaurantController.php
+++ server/app/Http/Controllers/RestaurantController.php
@@ -4,12 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Restaurant;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $restaurants = Restaurant::all();
+        $name = $request->name;
+        $category = $request->category;
+
+        $query = Restaurant::query();
+
+        if($name) {
+            $query->where('name', 'like', '%' . $name . '%');
+        }
+        if($category) {
+            $query->where('category', 'like', '%' . $category . '%');
+        }
+
+        $restaurants = $query->simplePaginate(10);
+        $restaurants->appends(compact('name', 'category'));
+
+        // 検索方法(拡張性がない)
+        // if (!empty($name)) {
+        //     $restaurants = Restaurant::where('name', 'like', '%' . $name . '%');
+        // } else {
+        //     $restaurants = Restaurant::all();
+        // }
+
+        // $restaurants = Restaurant::simplepaginate(10);
+
+        // $restaurants = DB::table('restaurants')
+        //     ->orderByRaw('recommendations IS NULL ASC')
+        //     ->orderBy('recommendations', 'ASC')
+        //     ->get();
+
         return view('restaurants.index', compact('restaurants'));
     }
 
